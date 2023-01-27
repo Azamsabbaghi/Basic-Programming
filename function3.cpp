c03dccdc5d6244ea331f65b0bb7411a66659f053
Azamsabbaghi <118680057+Azamsabbaghi@users.noreply.github.com>
2023-01-27 16:22:54 +0100

    Add files via upload

---
+++ function3.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int num,a,b,sum;
+cout<< "enter number :";
+cin>> num;
+a= num%10;
+b=num/10;
+sum=a+b;
+cout<<"sum is :"<<sum;
+                                                                      
+getch();
+
+    return 0;
+}
+
+
+
