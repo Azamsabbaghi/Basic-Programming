459e7e0ac0548071bf5a086e7e81ed8c325e2fe2
Azamsabbaghi <118680057+Azamsabbaghi@users.noreply.github.com>
2023-01-27 16:23:25 +0100

    Add files via upload

---
+++ function4.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int num1, num2,sum;
+cout<< "enter 2 number:"<<endl;
+cin>> num1>>num2;
+sum=num1+num2;
+cout<< "sum is:"<<sum;
+
+getch();
+    return 0;
+}
+
+
+
