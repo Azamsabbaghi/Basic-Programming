3a537c8034d75e6563d5bdc7a03635b6e9ffdb22
Azamsabbaghi <118680057+Azamsabbaghi@users.noreply.github.com>
2023-01-27 16:22:31 +0100

    Add files via upload

---
+++ function2.cpp
@@ -0,0 +1,25 @@
+
+ #include <iostream>
+ #include <conio.h>
+ using namespace std;
+ int main()
+{
+int num;
+cin>>num;
+ cout<<num;
+	
+	
+	int number;
+	number=500;
+	cout<< number<<endl;
+	float pi=23.55;
+	cout<<pi<<endl;
+	cout <<12<<endl<<3.8<<endl<<"ali"<<endl<<'$';
+  cout <<12;
+	 cout<< endl;
+    cout <<3.45;
+    cout<< endl;
+   cout <<"ali";
+    cout<< endl;
+     return 0;
+}
