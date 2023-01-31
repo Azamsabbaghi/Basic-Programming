a69596d8be876018b8b900dd15eb08d325833613
Azamsabbaghi <118680057+Azamsabbaghi@users.noreply.github.com>
2023-01-31 08:42:58 +0100

    Add files via upload

---
+++ multipleTable.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+void multipleTable(){
+
+for(int i=1; i<=10; i++){
+	for(int j=1; j<=10; j++){
+		cout<<i*j<<"\t";
+	}
+	cout<<endl;
+}
+}
+
+int main()
+{
+	multipleTable();
+	
+	
+	
+	getch();
+
+    return 0;
+}
