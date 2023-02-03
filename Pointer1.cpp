0acc780fc49e9d986b6773587dd268e3c176ede7
Azamsabbaghi <118680057+Azamsabbaghi@users.noreply.github.com>
2023-02-03 11:14:09 +0100

    Add files via upload

---
+++ Pointer1.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int num;
+	num=100;
+	cout<<num<<endl;
+	cout<<&num;
+	
+	                                                                      
+getch();
+
+    return 0;
+}
+
